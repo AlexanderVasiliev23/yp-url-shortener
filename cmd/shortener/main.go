3129ec58fb4f1550d5acac4fcb32630ec7aa85fc
alexandervasiliev23 <alexandervasiliev23@gmail.com>
2023-11-05 09:20:39 +0300

    Server, storage and hardcoded url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,81 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const (
+	addr = "localhost:8080"
+)
+
+var (
+	ErrUrlNotFound = errors.New("url is not found")
+)
+
+type localStorage map[string]string
+
+func newStorage() localStorage {
+	return make(localStorage)
+}
+
+func (s localStorage) add(token, url string) {
+	s[token] = url
+}
+
+func (s localStorage) get(token string) (string, error) {
+	url, ok := s[token]
+	if ok {
+		return url, nil
+	}
+
+	return "", ErrUrlNotFound
+}
+
+func main() {
+	storage := newStorage()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			token := strings.Trim(r.URL.Path, "/")
+
+			url, err := storage.get(token)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			w.Header().Set("Location", url)
+			w.WriteHeader(http.StatusTemporaryRedirect)
+
+			return
+		}
+
+		if r.Method == http.MethodPost {
+			url, err := io.ReadAll(r.Body)
+			if err != nil || len(url) == 0 {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+
+			token := "EwHXdJfB"
+			storage.add(token, string(url))
+
+			w.WriteHeader(http.StatusCreated)
+			fmt.Fprintf(w, "http://%s/%s", addr, token)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	})
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AlexanderVasiliev23/yp-url-shortener
+
+go 1.21.3
